8bef338c60e7253ca152b34729d2462b5037e01b
Rasel19997 <107494428+Rasel19997@users.noreply.github.com>
2022-06-28 11:22:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$('.one').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+$('.two').owlCarousel({
+    loop:true,
+    margin:10,
+    
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
